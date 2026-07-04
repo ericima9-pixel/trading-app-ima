02ce755e11c3cec74ee9411a681e6556783ce6d1
ericima9-pixel <ericima9@gmail.com>
2026-07-04 08:22:21 +0000

    Ajout du service worker

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "routine-trading-ima-v1";
+const SHELL_FILES = [
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  const estDonnee = url.hostname.includes("google.com") || url.hostname.includes("googleusercontent.com");
+
+  if (estDonnee) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
